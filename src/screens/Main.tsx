30389a126c1b13956089a0750abef68852a058b9
SangHan <discegaudere24.7@gmail.com>
2022-02-04 23:58:52 +0900

    Design: complete Main without content

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import { Box, Button, Container, Heading, Image, List, ListItem, Link } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, Image, List, ListItem, Link, useColorModeValue, Icon } from '@chakra-ui/react';
 import { Navbar } from './Navbar';
 import { NavLink } from 'react-router-dom';
 import styled from "@emotion/styled";
 import Section from '../components/Section';
+import { IoLogoGithub } from 'react-icons/io5'
+import { SiGmail } from 'react-icons/si'
+
 
 
 const Main = () => {
 	return (
 		<Box h="100vh">
 			<Container maxW="container.md" pt={14}>
-				<Box
-					borderRadius="lg"
-					mt={10}
-					mb={6}
-					p={5}
-					textAlign="center"
-				>
-					한줄로 소개하기
-				</Box>
 
-				<Box display={{ md: "flex" }}>
-					<Box flexGrow={2}>
-						<Heading as="h2">송상한</Heading>
+				<Box display="flex" justifyContent="space-between" pt={10}>
+					<Box
+						w="90%"
+						borderRadius="lg"
+						mt={10}
+						mb={6}
+						p={5}
+						textAlign="center"
+						bg={useColorModeValue('#EDF2F7', 'whiteAlpha.200')}
+					>
+						한줄로 소개하기
 					</Box>
 
 					<Box
-						mt={{ base: 4, md: 0}}
-						ml={{ md: 6 }}
+						mt={7}
+						ml={3}
 						textAlign="center"
 					>
 						<Image
@@ -43,28 +45,38 @@ const Main = () => {
 					</Box>
 				</Box>
 
-				<Section delay={0.1}>
-					<Heading as='h3'>
+				<Section delay={0.1} >
+					<Heading as='h3' mt={5} mb={2}>
 						소개
 					</Heading>
 					<Paragraph>
-						자기소개 글
+						한글입술으로 테스트.넣는 뭇 가슴에 것이다. 사는가 우리는 살 풀이 찾아 듣는다. 
+						같이, 청춘은 넣는 속잎나고, 따뜻한 봄바람을 충분히 모래뿐일 것이다. 
+						불어 인생에 보이는 간에 희망의 인류의 것이다. 열락의 착목한는 어디 맺어, 
+						하였으며, 황금시대다.
+						기술스택, 영어, 취미 등등 적기.
 					</Paragraph>
 
-					<Box alignItems='center' my={4}>
+					<Box align='center' my={4}>
 						<NavLink to='/works'>
-							<Button colorScheme="blue">My portfolio</Button>
+							<Button colorScheme="blue">My portfolio </Button>
 						</NavLink>
 					</Box>
 				</Section>
 
 				<Section delay={0.2}>
-					<Heading as="h3" pb={1}>경력</Heading>
+					<Heading as="h3" pb={2}>경력</Heading>
 					<Box pl="3.4rem">
-						<Year>1997</Year> 탄생
+						<Year>2016</Year> 부경대학교 IT융합응용공학과 입학
 					</Box>
 					<Box pl="3.4rem">
-						<Year>2016</Year> 부경대학교 입학
+						<Year>2021</Year> 캡디
+					</Box>
+					<Box pl="3.4rem">
+						<Year>2021</Year> 수영구 해커톤
+					</Box>
+					<Box pl="3.4rem">
+						<Year>2021</Year> 정보처리기사
 					</Box>
 				</Section>
 
@@ -75,22 +87,22 @@ const Main = () => {
 							<Link href='https://github.com/siiiido' >
 								<Button
 									variant="ghost"
-									colorScheme="blue"
-								>
-									이모티콘
+									leftIcon={<Icon as={IoLogoGithub} />}
+									ml={9}
+									>
+									Github
 								</Button>
-								깃허브
 							</Link>
 						</ListItem>
 						<ListItem>
-							<Link href='' >
+							<Link to='#'  >
 								<Button
 									variant="ghost"
-									colorScheme="blue"
-								>
-									이모티콘
+									leftIcon={<Icon as={SiGmail} />}
+									ml={9}
+									onClick={() => window.location.href = 'mailto:discegaudere24.7@gmail.com'}
+								>discegaudere24.7@gmail.com
 								</Button>
-								이메일
 							</Link>
 						</ListItem>
 					</List>
