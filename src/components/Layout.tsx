2475a794c15be271c361c13f8e37359450dd47e3
SangHan <discegaudere24.7@gmail.com>
2022-02-04 16:28:02 +0900

    Feat: create Layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { ChildrenProps } from '../type';
+
+const variants = {
+	hidden: { opacity: 0, x: 0, y: 20 },
+	enter: { opacity: 1, x: 0, y: 0 },
+	exit: { opacity: 0, x: -0, y: 20 }
+}
+
+const Layout = ({ children }: ChildrenProps) => {
+	return (
+		<motion.article
+			variants={variants}
+			initial="hidden"
+			animate="enter"
+			exit="exit"
+			transition={{ duration: 0.4, type: 'easeInOut' }}
+			style={{ position: 'relative' }}
+		>
+			{children}
+		</motion.article>
+	)
+};
+
+export default Layout;
