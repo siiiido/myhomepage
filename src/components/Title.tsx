6b78b4d0822aa3a4acdd168b3ca2bcd6dde6ecc5
SangHan <discegaudere24.7@gmail.com>
2022-02-04 16:55:28 +0900

    Feat: create Title

---
+++ src/components/Title.tsx
@@ -0,0 +1,22 @@
+import { NavLink } from 'react-router-dom';
+import { Heading, Box, Link } from "@chakra-ui/react";
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { ChildrenProps } from '../type';
+
+const Title = ({ children }: ChildrenProps) => {
+	return (
+		<Box>
+			<NavLink to='/works'>
+				<Link>Works</Link>
+			</NavLink>
+			<span> {' '}
+				<ChevronRightIcon/>{' '}
+			</span>
+			<Heading display="inline-block" as="he" fontSize={20} mb={4}>
+				{children}
+			</Heading>
+		</Box>
+	);
+};
+
+export default Title;
