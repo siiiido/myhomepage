e8375a3eb3fe21b3389812dee1eca6b562b8004f
SangHan <discegaudere24.7@gmail.com>
2022-02-04 17:02:20 +0900

    Feat: add Works 페이지 세부내용

--- src/screens/works/NewSpring.tsx
+++ src/screens/works/NewSpring.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Badge, Container, ListItem, List } from '@chakra-ui/react';
+import Layout from '../../components/Layout';
+import { Navbar } from '../Navbar';
+import Title from '../../components/Title';
+import { Paragraph } from '../Main';
 
 const NewSpring = () => {
-	return <div></div>;
+	return (
+		<Layout>
+			<Navbar/>
+			<Container pt={20}>
+				<Title>
+					새봄 <Badge>날짜</Badge>
+				</Title>
+				<Paragraph>
+					새봄 설명
+				</Paragraph>
+				<List>
+					<ListItem>
+						<span>세부설명1</span>
+					</ListItem>
+					<ListItem>
+						<span>세부설명2</span>
+					</ListItem>
+					<ListItem>
+						<span>세부설명3</span>
+					</ListItem>
+				</List>
+				
+			</Container>
+		</Layout>
+	);
 };
 
 export default NewSpring;
--- src/screens/works/SuyeongHackathon.tsx
+++ src/screens/works/SuyeongHackathon.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Badge, Container, ListItem, List } from '@chakra-ui/react';
+import Layout from '../../components/Layout';
+import { Navbar } from '../Navbar';
+import Title from '../../components/Title';
+import { Paragraph } from '../Main';
 
 const SuyeongHackathon = () => {
-	return <div></div>;
+	return (
+		<Layout>
+			<Navbar/>
+			<Container pt={20}>
+				<Title>
+					어서와수영구는처음이지 <Badge>날짜</Badge>
+				</Title>
+				<Paragraph>
+				어서와수영구는처음이지 설명
+				</Paragraph>
+				<List>
+					<ListItem>
+						<span>세부설명1</span>
+					</ListItem>
+					<ListItem>
+						<span>세부설명2</span>
+					</ListItem>
+					<ListItem>
+						<span>세부설명3</span>
+					</ListItem>
+				</List>
+		</Container>
+	</Layout>
+	);
 };
 
 export default SuyeongHackathon;
