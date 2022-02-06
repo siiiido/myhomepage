5d4f3fbdc0f22488c5d512d593b294b5b5735451
SangHan <discegaudere24.7@gmail.com>
2022-02-06 16:14:03 +0900

    Fix: transition type error

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,26 +1,15 @@
-import React, { ReactChild, ReactChildren } from 'react';
 import { motion } from 'framer-motion';
-import { shouldForwardProp, chakra } from '@chakra-ui/react';
 import { SectionProps } from '../type';
-
-
-const StyleDiv = chakra(motion.div, {
-	shouldForwardProp: props => {
-		return shouldForwardProp(props) || props === 'transition'
-	}
-});
-
 
 const Section= ({ children, delay=0 }: SectionProps) => {
 	return (
-		<StyleDiv
+		<motion.div
 			initial={{ y: 10, opacity: 0 }}
 			animate={{ y: 0, opacity: 1 }}
-			// transition={{ duration: 0.8, delay}}
-			mb={6}
+			transition={{ duration: 0.8, delay}}
 		>
 			{children}
-		</StyleDiv>
+		</motion.div>
 	)
 };
 
