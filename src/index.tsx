10bf12cb64024cdf752b2bb2836bf6634cf7b22e
siiiido <discegaudere24.7@gmail.com>
2022-02-10 23:27:54 +0900

    Design: add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react'
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Open Sans',
+    body: 'Raleway',
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
